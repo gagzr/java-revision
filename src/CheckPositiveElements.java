cacb07ede1ef21b3154bfbb93f75496e66ff4d74
gagzr <rajshawgagan@gmail.com>
2026-08-20 22:42:40 +0530

    Add: desc for allMatch()

--- src/CheckPositiveElements.java
+++ src/CheckPositiveElements.java
@@ -4,7 +4,8 @@ public class CheckPositiveElements {
     public static void main(String[] args){
         //List.of = Create a list
         // .stream()= convert the list to stream
-        // .allMatch(takes arg)
+        // .allMatch() = terminal operation that checks
+        // if every element in the stream satisfies the given condition
         boolean allPositive = List.of(1,2,3,4,5).stream().allMatch(n -> n>0);
         System.out.println(allPositive);
     }
