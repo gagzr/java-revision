45120e696c073d45261159b4ac7527e197335a0c
gagzr <rajshawgagan@gmail.com>
2026-08-20 23:11:13 +0530

    Convert List of Strings to Map using Collectors

--- src/ConvertListOfStringsToMap.java
+++ src/ConvertListOfStringsToMap.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ConvertListOfStringsToMap {
+    public static void main(String[] args){
+        Map<String, Integer> map= List.of("Java","Go","Bash","SQL").stream()
+                .collect(Collectors.toMap(w-> w,String::length));
+        System.out.println(map);
+        //Collectors : utility class that provides pre-defined static method to gather stream data
+        // common methods include .toList(), .toMap(), .toSet()
+    }
 }
