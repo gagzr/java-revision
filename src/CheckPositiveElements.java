991f895bc1a01c563fb4fdff56c531a30b32fafd
gagzr <rajshawgagan@gmail.com>
2026-08-20 22:38:39 +0530

    Update gitignore

--- src/CheckPositiveElements.java
+++ src/CheckPositiveElements.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class CheckPositiveElements {
+    public static void main(String[] args){
+        //List.of = Create a list
+        // .stream()= convert the list to stream
+        // .allMatch(takes arg)
+        boolean allPositive = List.of(1,2,3,4,5).stream().allMatch(n -> n>0);
+        System.out.println(allPositive);
+    }
 }
