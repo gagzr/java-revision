05a5491ef931e43a9d040ca986e90c50ba497f96
gagzr <rajshawgagan@gmail.com>
2026-08-20 22:44:17 +0530

    Update : desc for List.of()

--- src/CheckPositiveElements.java
+++ src/CheckPositiveElements.java
@@ -2,10 +2,11 @@ import java.util.List;
 
 public class CheckPositiveElements {
     public static void main(String[] args){
-        //List.of = Create a list
+        //List.of = Static factory method to Create an immutable list
         // .stream()= convert the list to stream
         // .allMatch() = terminal operation that checks
         // if every element in the stream satisfies the given condition
+        // return true or false
         boolean allPositive = List.of(1,2,3,4,5).stream().allMatch(n -> n>0);
         System.out.println(allPositive);
     }
