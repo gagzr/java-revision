3440043fc15bcb31ee2c6fa989325320e7d71494
gagzr <rajshawgagan@gmail.com>
2026-08-20 22:55:04 +0530

    Convert List of Strings to UpperCase

--- src/ConvertAllStringsToUpperCase.java
+++ src/ConvertAllStringsToUpperCase.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class ConvertAllStringsToUpperCase {
+    public static void main(String[] args){
+        List<String> names = List.of("alice","bob","charlie");
+        // .map() = Used to transform stream data
+        // String::toUpperCase = Method Reference, shortand to (String s)-> s.toUpperCase()
+        // :: known as reference Operator, often used with functional interfaces
+        // .toList() used to collect elements of a stream to an immutable list
+        List<String> upper = names.stream().map(String::toUpperCase).toList();
+        System.out.println(upper);
+    }
 }
